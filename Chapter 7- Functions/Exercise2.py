01b5f821b36f4385633dedb67412302518b8e834
Percyana552 <146834470+Percyana552@users.noreply.github.com>
2023-10-31 23:12:37 +0400

    Add files via upload

---
+++ Chapter 7- Functions/Exercise2.py
@@ -0,0 +1,8 @@
+"""Write a function called favorite_book() that accepts one parameter, title.
+ The function should print a message, such as One of my favvorite book """
+
+def favorite_book(book):
+    print(book+ " is one of my favorite book")
+
+
+favorite_book("how to talk to anyone")

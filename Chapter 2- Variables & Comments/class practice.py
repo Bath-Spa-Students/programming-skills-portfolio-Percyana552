9be9d80b3f458054e415eaac6c8d027760a8d44a
Percyana552 <146834470+Percyana552@users.noreply.github.com>
2023-11-12 07:01:07 +0400

    class practice.py

---
+++ Chapter 2- Variables & Comments/class practice.py
@@ -0,0 +1,29 @@
+class practice:"py"
+#creating variable
+x=10
+y="Percyana"
+print(x)
+print(y)
+# variable do not need to be declared
+x=12
+x="Rafia"
+print(x)
+#casting
+x=str(190)
+y=int(100)
+z=float(10)
+#get the data type 
+x=20
+y="fernada"
+print(type(x))
+print(type(y))
+# case-sensiteve language
+a=15
+A="jordan"
+print(a)
+print(A)
+#single or double quotes, is the same thing.
+x="jordan"
+# is the same as
+x='jordan'
+print(x)

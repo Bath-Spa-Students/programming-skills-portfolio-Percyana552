da3fa321d4e5c0cf9d4fa1a298c37e131faa6492
Percyana552 <146834470+Percyana552@users.noreply.github.com>
2023-11-12 07:06:18 +0400

    Add files via upload

--- Chapter 2- Variables & Comments/class practice.py
+++ Chapter 2- Variables & Comments/class practice.py
@@ -1,29 +1,51 @@
 class practice:"py"
-#creating variable
-x=10
-y="Percyana"
-print(x)
-print(y)
-# variable do not need to be declared
-x=12
-x="Rafia"
-print(x)
-#casting
-x=str(190)
-y=int(100)
-z=float(10)
-#get the data type 
-x=20
-y="fernada"
-print(type(x))
-print(type(y))
-# case-sensiteve language
-a=15
-A="jordan"
+# strings
+print("hello")
+# assign string to a variable
+a="hey"
 print(a)
-print(A)
-#single or double quotes, is the same thing.
-x="jordan"
-# is the same as
-x='jordan'
-print(x)
+""" multiline strings.
+    you can assign a multiline string to a variable 
+    by using by using three quoutes or single qoutes:"""
+a=""" you must be the change
+      you wish to see in the world.
+      accept yourself, love yourself
+      and keep moving forward,if you want
+      to fly, you have to give up what
+      weighs you down."""
+print(a)
+# strings are arrays
+a="hey , Petras!"
+print(a[1])
+# string length
+a="hey,Petras!"
+print(len(a))
+
+#slicing strings
+b="hey,sweetheart"
+print(b[1:5])
+# negative index
+b="hey,sweetheart"
+print(b[-3:-1])
+#modify strings
+# upper case
+a="hello , sweets!"
+print(a.upper())
+# lower case
+a="HELLO,SWEETS"
+print(a.lower())
+"""  remove whitespace.The strip()method removes 
+     any whitespace from the beginning or the end"""
+a="hello,sweets!"
+print(a.strip())
+# replace string
+a="Hey, sweets!"
+print(a.replace("y","i"))
+#split string
+a="Hello ,sunshine!"
+print(a.split(","))
+# concatenate
+a="hey"
+# list allow duplicate
+items=["book"," box of water","wallet"]
+print(items)
